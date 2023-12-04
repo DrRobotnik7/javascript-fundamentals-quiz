3720522c693ed5d3fb2808fe674691a4146ca923
Dr.Robotnik <jackj.1988@googlemail.com>
2023-12-04 20:51:33 +0000

    added comments

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,12 @@
-// Helper functions
+// Helper functions to save using same code multiple times
 function show(element){
   element.classList.remove("hide");
 }
+
 function hide(element){
   element.classList.add("hide");
 }
+
 function getData(key) {
   const v = localStorage.getItem(key);
   if(!v) {
@@ -12,10 +14,12 @@ function getData(key) {
   }
   return JSON.parse(v);
 }
+
 function setData(key, value) {
   return localStorage.setItem(key, JSON.stringify(value));
 }
 
+// Fetching HTML elements and assigning them to variables so they can be interacted with and manipulated in JavaScript
 const $time = document.getElementById("time");
 const $questions = document.getElementById("questions");
 const $questionTitle = document.getElementById("question-title");
@@ -28,36 +32,45 @@ const $finalScore = document.getElementById("final-score");
 const $initials = document.getElementById("initials");
 const $submit = document.getElementById("submit");
 
+// Declaring variables globally so they can be accessed by functions in forthcoming logic
 let feedbackTimeout;
 let currentTime;
 let timer;
 let currentQuestion;
 let currentScore = 0;
 
+// If this is the first attempt the users score object is set as an empty array here. If it is not the first attempt, the variable now stores all of the data held in local storage 
 let userScore = getData('userScore') || [];
 
+// When the start button is clicked the timer countdown begins and the first question is shown
 $start.addEventListener("click", function() {
   startTimer();
   presentNextQuestion();
 });
 
+// When the submit button is clicked at the end of the quiz the users initials and score are added to an array of user scores in the local storage
 $submit.addEventListener("click", function() {
   userScore.push({
     'initials': $initials.value,
     'highScore': currentScore
   });
   
+  // The user scores object array is now updated
   setData('userScore', userScore)
+
+  // The user is taken from the quiz to the high scores
   window.location.href = "highscores.html";
 });
 
+// This code block sets the timer to 75 seconds and intialises the score to 0. It hides the start screen and shows the question
 function startTimer() {
-  currentQuestion = -1;
+  currentQuestion = -1; // This is set to -1 so that when 1 is added for the first question, this will be index 0 of the questions array
   currentTime = 75;
   currentScore = 0;
   show($questions);
   hide($startScreen);
   
+  // This sets the timer to decrement by 1 every second - it also triggers the gameOver function when the timer gets to 0
   timer = setInterval(function(){
     currentTime -= 1;
     $time.innerHTML = currentTime
@@ -66,6 +79,8 @@ function startTimer() {
     } 
   }, 1000);
 }
+
+// When the timer reaches 0, the timer stops decrementing, the quiz disappears and the end screen is shown to the user with their score and an initials input box
 function gameOver() {
   clearInterval(timer);
   $finalScore.innerHTML = currentScore;
@@ -74,13 +89,14 @@ function gameOver() {
   show($endScreen);
 }
 
+// This function builds the next question and answer buttons from the array of questions
 function presentNextQuestion() {
   currentQuestion += 1;
   if(currentQuestion === questions.length) {
     gameOver();
-    return;
+    return; // This return stops the rest of the function from being executed if the quiz ends
   }
-  $choices.innerHTML = "";
+  $choices.innerHTML = ""; // This clears the previous questions multiple choice options
   $questionTitle.innerHTML = questions[currentQuestion]["question"];
   for (let i=0; i < questions[currentQuestion]["options"].length; i++){
     const $optionButton = document.createElement("button");
@@ -90,26 +106,27 @@ function presentNextQuestion() {
   }
 }
 
+// This function informs the user whether they got the answer correct or incorrect and plays the associated sound.
 function pickAnswer(event) {
   var audio;
   clearTimeout(feedbackTimeout);
 
   if(questions[currentQuestion]["answer"] !== event.srcElement.innerHTML) {
-    currentTime -= 10;
+    currentTime -= 10; // If wrong the time is reduced by 10 seconds
     $feedback.innerHTML = "Wrong!";
     audio = new Audio('./assets/sfx/incorrect.wav');
   } else {
     $feedback.innerHTML = "Correct!";
-    currentScore += 1;
+    currentScore += 1; // If correct the users current score increases by 1
     audio = new Audio('./assets/sfx/correct.wav');
   }
   audio.play();
   
-  show($feedback);
+  show($feedback); // This shows the user whether they got the question right or wrong
 
   feedbackTimeout = setTimeout(function() {
     hide($feedback);
-  }, 4000);
-  presentNextQuestion();
+  }, 4000); // This hides the feedback after 4 seconds
+  presentNextQuestion(); // This triggers the next question function
 }
 
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,7 +2,6 @@ const $highScores = document.getElementById("highscores");
 const $clear = document.getElementById("clear");
 let userScore = getData('userScore');
 
-// Helper functions
 function getData(key) {
   const v = localStorage.getItem(key);
   if(!v) {
@@ -11,11 +10,14 @@ function getData(key) {
   return JSON.parse(v);
 }
 
+// This for loop creates a list element, injects the text showing the users initials and score and appends to the high scores list
 for(var i=0; i<userScore.length; i++) {
   let $li = document.createElement("li");
   $li.innerHTML = "Initials: " + userScore[i]['initials'] + " Score: " + userScore[i]['highScore'];
   $highScores.appendChild($li);
 }
+
+// This clears the list of high scores if the user clicks on 'Clear Highscores' - the user is then redirected to the start of the quiz
 
 $clear.addEventListener("click", function() {
   localStorage.clear();
